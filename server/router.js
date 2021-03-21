79b551b3cc32d6370188e1824c641a3c2608b65d
xiongfeng <343138759@qq.com>
2021-03-21 15:48:59 +0800

    删除了后台router

--- server/router.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-var Info = require('../model/info.js')
-console.log('1')
-router.get('/', function(req, res) {
-    console.log('2')
-
-})
-module.exports = router;
